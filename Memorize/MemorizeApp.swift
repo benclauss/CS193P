de4d3b390c042c31017cbee85466cc11164731e6
Benjamin Clauss <benjaminclauss@protonmail.com>
2023-03-25 15:39:32 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Benjamin Clauss on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
